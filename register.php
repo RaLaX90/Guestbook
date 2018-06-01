ea9ee257ce6672aa766d8cb34ee27d3b38f45fb5
Grand Master <gmastrbit@gmail.com>
2018-06-01 15:56:37 +0300

    Google Analytics
    Add some code

--- register.php
+++ register.php
@@ -61,6 +61,14 @@
 <html>
     <head> 
         <title> Гостьова книга </title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120229196-3"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-120229196-3');
+        </script>
         <link rel="stylesheet" href="style.css">
         <link rel="shortcut icon" href="ico.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
